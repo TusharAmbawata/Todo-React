1f2754bea7cdbbf40e4f552e88c500654fa9f243
TusharAmbawata <ambawatatushar@gmail.com>
2025-09-20 20:39:21 +0530

    Update TodoList file

--- src/Section/TodoList.jsx
+++ src/Section/TodoList.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
-    <div className='container'>
-      <h2>Get Todo</h2>
+    <div className='container py-2'>
+      <h2 className='my-3'>Todo's</h2>
+      <div className="d-flex flex-wrap">
+      {todos.map((item) =>(
+        <div class="card m-3 " style={{ width: "18rem"}}>
+          <div class="card-header">
+            {item.todo}
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">{item.tittle}</h5>
+            <p class="card-text">{item.description}</p>
+            <a href="#" class="btn btn-primary">Edit</a>
+            <a href="#" class="btn btn-danger mx-2">Delete</a>
+          </div>
+        </div>
+          ))}
+          </div>
     </div>
   )
 }
