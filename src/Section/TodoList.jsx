6adb6ceb0bc0616fe7f455397164a59ae1256ff1
TusharAmbawata <ambawatatushar@gmail.com>
2025-09-24 21:11:30 +0530

    Update TodoList.jsx

--- src/Section/TodoList.jsx
+++ src/Section/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, deleteTodo }) => {
   return (
     <div className='container py-2'>
       <h2 className='my-3'>Todo's</h2>
@@ -8,8 +8,8 @@ const TodoList = ({ todos }) => {
         <p className="text-muted">No todos yet. Add one!</p>
       ):(
       <div className="d-flex flex-wrap">
-      {todos.map((item) =>(
-        <div class="card m-3 " style={{ width: "18rem"}}>
+      {todos.map((item,index) =>(
+        <div class="card m-3 " style={{ width: "18rem"}} key={index}>
           <div class="card-header">
             {item.todo}
           </div>
@@ -17,7 +17,9 @@ const TodoList = ({ todos }) => {
             <h5 class="card-title">{item.tittle}</h5>
             <p class="card-text">{item.description}</p>
             <a href="#" class="btn btn-primary">Edit</a>
-            <a href="#" class="btn btn-danger mx-2">Delete</a>
+            <button  class="btn btn-danger mx-2"
+            onClick={()=>deleteTodo(index)}
+            >Delete</button>
           </div>
         </div>
           ))}
